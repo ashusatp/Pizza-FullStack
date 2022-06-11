e704861ebb3ec6bcacea5dd77869381bc54794a9
Ashu <ashutoshworldking1234@gmail.com>
2022-06-11 23:42:02 +0530

    add cart page, auth page

--- server.js
+++ server.js
@@ -8,15 +8,28 @@ let PORT = process.env.PORT || 3000;
 
 //Assets
 app.use(express.static('public'))
-app.get('/' , (req, res) =>{
-    res.render('home')
-});
+
 
 
 // set Template engine
 app.use(expressLayout);
 app.set('views' , path.join(__dirname , 'resources' , 'views'));
 app.set('view engine' , 'ejs');
+
+
+app.get('/' , (req, res) =>{
+    res.render('home')
+});
+app.get('/cart' , (req, res) =>{
+    res.render('customers/cart.ejs')
+});
+app.get('/register' , (req, res) =>{
+    res.render('auth/register.ejs')
+});
+app.get('/login' , (req, res) =>{
+    res.render('auth/login.ejs')
+});
+
 
 app.listen( PORT , () =>{
     console.log(`listening on port ${PORT}`);
